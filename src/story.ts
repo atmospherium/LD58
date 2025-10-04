9f4b569aa6ae0dc3726355f6577f1b2d3027ab8f
jpennington_healthguard <jpennington@healthguardsecurity.com>
2025-10-03 22:25:42 -0400

    Initial Commit

---
+++ src/story.ts
@@ -0,0 +1,7 @@
+type StoryEntry = string;
+export const story: Array<StoryEntry> = [
+  "Protectors, Burdens, Traumas, Comforts",
+  "",
+  "We all have a history.",
+  "Who we are now is reflective of our past.",
+];
